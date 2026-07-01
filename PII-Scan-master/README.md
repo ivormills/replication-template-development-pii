c8e8987efbd55b6c17a93495ba93aade9e01b910
ivormills <iwm9@cornell.edu>
2026-07-01 17:50:22 -0400

    Add files via upload

---
+++ PII-Scan-master/README.md
@@ -0,0 +1,56 @@
+## Synopsis
+
+Scan.R searches all Stata (.dta), SAS (.sas7bdat), SPSS (.sav), and comma-separated values (.csv) files found in the specified directory for variables that may contain personally identifiable information (PII) using strings that commonly appear as part of variable names or labels that contain PII. (Note: Scan.R does not search labels in .csv files.) Results are displayed to the screen and saved to a comma-separated values file in the current working directory containing the variables and data flagged as potential PII.
+
+Note: Development on Scan.R has been temporarily halted. For an up-to-date version, see J-PAL's Stata PII Scanner (stata_PII_scan) at https://github.com/J-PAL/stata_PII_scan.
+
+## Instructions
+
+To execute the script, type `Rscript scan.R --path=[Target]` into the command line, replacing [Target] with the folder containing the data to scan. An excel spreadsheet titled "PII_output" will be saved in the current working directory for your review.
+
+##### Options
+* `--quiet`: Silent operation; do not display possible PII to the screen
+* `--no-output`: Do not output search results to CSV file
+* `--output-file=output_filename.csv`: Write csv of possible PII to file "output_filename.csv" instead of "PII_output.csv"
+* `--strict`: Use strict matching when comparing strings. For example, match "lat" but not "latin"
+* `--nolabels`: Do not scan variable labels for PII search terms
+
+## Requirements
+
+* R must be installed on the local system. Installers can be downloaded at http://cran.us.r-project.org.
+* The dplyr, purrr, optparse, rio and tools packages are required. To install missing packages, run install.packages("PACKAGE_NAME") where PACKAGE_NAME is the name of the package to install. Scan.R will warn if any of these packages are missing.
+
+## Exit Codes
+scan.R will exit with a value of 10 (ten) when possible PII is found or a value of 0 (zero) when no PII is identified.
+
+## Motivation
+
+The script was written to audit data files for personally identifiable information. It provides a solution to quickly searching a large number of files in a particular directory or searching files that contain a large number of variables. However, it does not fully replace manual detection of PII.
+
+## Search Strings
+
+scan.R searches variables and labels for the following strings:
+ address, bday, beneficiary, birth, birthday, block, census, child, city, community, compound, coord, country, daughter, degree, district, dob, email, father, fax, first_name, fname, gender, gps, house, husband, last_name, lat, lname, loc, location, lon, minute, mother, municipality, name, network, panchayat, parish, phone, precinct, school, second, sex, social, spouse, son, street, subcountry, territory, url, village, wife, zip
+ 
+ 
+
+## Support
+
+Running this script on a Windows machine may result in the following error:
+~~~~
+Error in df_parse_dta_file(spec, encoding) :
+  Failed to parse [pathname]: Unable to allocate memory.
+Calls: import ... standardize_attributes -> read_dta -> df_parse_dta_file -> .Call
+Execution halted
+~~~~
+
+If you receive this error, remove the current version of the R package "Haven" and install the 1.1.0 version as a local zip file. The older version can be downloaded at: https://mran.microsoft.com/snapshot/2017-11-01/bin/windows/contrib/3.5/.
+
+Please use the [issue tracker](https://github.com/J-PAL/PII-Scan/issues) for all support requests.
+
+## License
+
+See [license file](LICENSE.txt).
+
+## Thanks
+Special thanks to IPA for their [How to Search Datasets for Personally Identifiable Information](http://www.poverty-action.org/sites/default/files/Guideline_How-to-Search-Datasets-for-PII.pdf) document which inspired this project.
---
+++ PII-Scan-master/scan.R
@@ -0,0 +1,308 @@
+# List of required packages
+#   optparse: command line options/arguments
+#   rio: load files as data frame
+#   tools: extract file extension
+packages = c("dplyr",
+             "purrr",
+             "optparse",
+             "rio",
+             "tools")
+
+# Suppress warnings
+oldw <- getOption("warn")
+options(warn = -1)
+
+# Check for required packages
+package.check <- lapply(
+  packages,
+  FUN = function(x) {
+    if ((!require(x, character.only = TRUE, quietly = TRUE,
+                  warn.conflicts = FALSE))) {
+      stop("Package ", x, " not found and is required.", call. = FALSE)
+    }
+  }
+)
+
+# Un-suppress warnings
+options(warn = oldw)
+
+# Set command line options
+option_list = list(
+  make_option(
+    c("-p", "--path"),
+    type = "character",
+    default = NULL,
+    help = "Path to search for PII",
+    metavar = "PATH"
+  ),
+  make_option(
+    c("-q", "--quiet"),
+    type = "logical",
+    action="store_true",
+    default = FALSE,
+    help = "Silent operation; do not display possible PII to the screen"
+  ),
+  make_option(
+    "--no-output",
+    dest = "nooutput",
+    type = "logical",
+    action="store_true",
+    default = FALSE,
+    help = "Do not output search results to csv file"
+  ),
+  make_option(
+    c("-o", "--output-file"),
+    dest = "outputfile",
+    type = "character",
+    default = "PII_output.csv",
+    metavar = "FILE",
+    help = "Write csv of possible PII to FILE [default: %default]"
+  ),
+  make_option(
+    c("-s", "--strict"),
+    type = "logical",
+    action="store_true",
+    default = FALSE,
+    help = "Use stric matching when comparing strings. For example, match 'lat' but not 'latin'"
+  ),
+  make_option(
+    c("-n", "--nolabels"),
+    type = "logical",
+    dest = "noscanlables",
+    action="store_true",
+    default = FALSE,
+    help = "Do not scan variable labels when checking for PII"
+  )
+)
+### In line 69 I had to update the old -nl option in scan.R to -n because the current optparse package rejects multi-character things.
+opt_parser = OptionParser(usage = "usage: %prog --path PATH [options]", option_list = option_list)
+
+opt = parse_args(opt_parser)
+
+# Make sure path is give as option
+if (is.null(opt$path)) {
+  print_help(opt_parser)
+  stop("A search path must be specified.", call. = FALSE)
+}
+
+# Make sure path is for a directory
+if (!dir.exists(opt$path)) {
+  stop("Path must secify a directory.", call. = FALSE)
+}
+
+# Set path
+path = opt$path
+
+# Set options
+strict = opt$strict
+quiet = opt$quiet
+outputCSV = !opt$nooutput
+outputfile = opt$outputfile
+no_scan_lables = opt$noscanlables
+
+# Set PII status
+PII_Found <- FALSE
+
+# Create prinf function
+printf <- function(...)
+  cat(sprintf(...))
+
+# Strings to look for in variable names
+pii_strings_names <-
+  c("name", "fname", "lname", "first_name", "last_name")
+pii_strings_dates <- c("birth", "birthday", "bday", "dob")
+pii_strings_locations <-
+  c(
+    "district",
+    "city",
+    "country",
+    "subcountry",
+    "parish",
+    "loc",
+    "street",
+    "village",
+    "community",
+    "address",
+    "gps",
+    "degree",
+    "minute",
+    "second",
+    "lat",
+    "lon",
+    "coord",
+    "location",
+    "house",
+    "compound",
+    "panchayat",
+    "territory",
+    "municipality",
+    "precinct",
+    "block",
+    "zip"
+  )
+pii_strings_other <-
+  c(
+    "school",
+    "social",
+    "network",
+    "census",
+    "gender",
+    "sex",
+    "fax",
+    "email",
+    "url",
+    "child",
+    "beneficiary",
+    "mother",
+    "wife",
+    "father",
+    "husband",
+    "phone",
+    "spouse",
+    "daughter",
+    "son"
+  )
+
+# Create single list of all strings, removing duplicates
+pii_strings <-
+  unique(c(
+    pii_strings_names,
+    pii_strings_dates,
+    pii_strings_locations,
+    pii_strings_other
+  ))
+
+
+# Get list of files (.dta or .sas7bdat) to scan for PII
+files = list.files(path,
+                   pattern = "\\.dta$|\\.sas7bdat$|\\.sav$|\\.csv$",
+                   recursive = TRUE)
+
+# Initialize output csv
+if (outputCSV) {
+  # Create data frame to hold PII matches
+  csv_headers <-
+    data.frame(
+      "file" = character(0),
+      "var" = character(0),
+      "varlabel" = character(0),
+      "samp1" = character(0),
+      "samp2" = character(0),
+      "samp3" = character(0),
+      "samp4" = character(0),
+      "samp5" = character(0)
+    )
+  write.csv(csv_headers, file = outputfile, quote=FALSE, row.names = FALSE)
+}
+
+# Loop over files
+for (file in files) {
+  
+  # Initialize variable count
+  v <- 0
+  
+  # Create full path to file
+  file <- file.path(path,file)
+  
+  # Get absolute path to file for cleaner output
+  file <- normalizePath(file)
+  
+  # Get file type
+  type <- file_ext(file)
+  
+  # Read file using rio
+  data <- import(file)
+  
+  # Move variable-level attributes to the data frame level
+  gather_attrs(data)
+  
+  # Loop over variable names in file
+  for (varname in names(data)) {
+    varlabel <- attr(data[[varname]],"label")
+    FOUND <- FALSE
+    
+    # Variable count for var.labels index
+    v <- v + 1
+    
+    for (string in pii_strings) {
+      
+      # Match on word boundary if strict
+      if (strict) {
+        string <- paste("\b",string,"\b")
+      }
+      
+      # Compare string to var, ignoring case
+      if (grepl(string, varname, ignore.case = TRUE)) {
+        FOUND <- TRUE
+      } else if ((!is.null(varlabel)) & !(no_scan_lables)) {
+        # If no possible PII found in variable name, check label, ignoring case
+        if (grepl(string, varlabel, ignore.case = TRUE)) {
+          FOUND <- TRUE
+        }
+      }
+    }
+    
+    if (FOUND) {
+      PII_Found <- TRUE
+      if (!quiet) printf("Possible PII found in %s:\n", file)
+      
+      # Print warning and first five non-missing, unique values:
+      if (!quiet) {
+        if (is.null(varlabel)) {
+          message <- paste("\"",varname,"\"",sep="")
+        } else {
+          message <- paste("\"",varname,"\" with label \"",varlabel,"\"", sep="")
+        }
+        printf("\tPossible PII in variable %s:\n", message)
+      }
+      
+      #Select the current variable column:
+      data_col <- data[varname]
+      
+      #Update the column to be character values:
+      data_col[varname] <- as.character(data_col[[varname]])
+      
+      #Select just the current variable:
+      var_only <- data_col[varname]
+      
+      #Select unique values of the variable:
+      var_unique <- unique(var_only)
+      
+      #Remove NA values from vector (will only paste NA values if there are fewer than 5 unique, non-missing values)
+      var_unique_nona <- na.omit(var_unique)
+      
+      # Print first five values
+      for (i in 1:5) {
+        if ((!quiet) & (!is.null(var_unique_nona[i,1])) & (!is.na(var_unique_nona[i,1]))) {
+          printf("\t\tSamp %d value: %s\n", i, var_unique_nona[i,1])
+        }
+      } # for ( i in 1:5 )
+      
+      # Print newline for readability
+      if (!quiet) printf("\n")
+      
+      # Write to csv file
+      if (outputCSV) {
+        # Create data frame without row names
+        new_row <- data_frame(
+          file = paste(file),
+          var = paste(varname),
+          varlabel = paste( if (is.null(varlabel)) "" else varlabel),
+          samp1 = paste(var_unique_nona[1,1]),
+          samp2 = paste(var_unique_nona[2,1]),
+          samp3 = paste(var_unique_nona[3,1]),
+          samp4 = paste(var_unique_nona[4,1]),
+          samp5 = paste(var_unique_nona[5,1])
+        )
+        write.table(new_row, file=outputfile, quote=TRUE, append=TRUE, row.names=FALSE, col.names=FALSE,  sep=",")
+      }
+      
+    } # if ( var %in% pii_strings )
+  } # for ( var in names( data ))
+} # for ( file in files )
+
+if ( PII_Found ) {
+  quit(save = "no", status = 10, runLast = FALSE)
+} else {
+  quit(save = "no", status = 0, runLast = FALSE)
+}
